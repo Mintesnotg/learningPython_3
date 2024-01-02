0a34376a783c7ae381e8e27cfbce0fc19ae1998f
Mintesnot Girma <MintesnotGi@ethiopianairlines.com>
2024-01-02 16:28:50 +0300

    workingwithdatetime

---
+++ remainingage.py
@@ -0,0 +1,11 @@
+import datetime
+
+age=int(input("enter your age\n"))
+
+days= (90-age) *365
+
+hours= days*24
+
+weeks=days//7
+
+print(f"you will live {days} days or {hours} hours or {weeks} weeks from now")
