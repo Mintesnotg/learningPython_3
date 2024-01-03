d924ecbcd774a93809a78d19e48e04ef2f14c22c
Mintesnot Girma <MintesnotGi@ethiopianairlines.com>
2024-01-03 09:54:33 +0300

    randomnums

--- pizza.py
+++ pizza.py
@@ -12,21 +12,28 @@ def pizzabilling(pizzasize, number):
     return bill
 
 
-def drink(drink, number):
+def drink_bill(drink, number):
 
     bill = 0
 
     match (drink):
-        case "cola":
+        case "1":
             bill += 10*number
-        case "sprite":
+        case "2":
             bill += 15*number
-        case "orange":
+        case "3":
             bill += 5*number
     return bill
 
 
 pizza_size = str(input("which pizza size do you want \n 1. for Large \n 2. for Medium \n 3. for Samll \n"))
 no_pizza = int(input('number of pizzas do you want \n'))
+
+drink = str(input("which drink size you want \n 1. for cola \n 2. for sprite \n 3. for orange \n"))
+no_drink = int(input('number of drink you want \n'))
 pizabill = pizzabilling(pizza_size, no_pizza)
+drinkbill = drink_bill(drink, no_drink)
+
 print(f" your pizza bill is {pizabill}$")
+print(f" your drink bill is {drinkbill}$")
+print(f" total bill  {drinkbill+pizabill}$")
---
+++ ramdom.py
@@ -0,0 +1,21 @@
+import random
+
+def suffuler (name):
+    
+
+    return random.choice(name)
+
+
+  
+
+
+ 
+
+
+name = str(input('enter your name\n'))
+
+sh=suffuler(name)
+
+print(f'your shuffled name {sh}')
+
+
