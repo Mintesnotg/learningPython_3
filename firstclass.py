54711fed29e8a53edec0e1f79dceb86cef73fc31
Mintesnot Girma <MintesnotGi@ethiopianairlines.com>
2024-01-10 20:48:32 +0300

    methodassignment

---
+++ firstclass.py
@@ -0,0 +1,26 @@
+def firstclass () :
+
+    scream = print
+
+    scream('eref beleye')
+
+
+firstclass()
+
+
+def counter ():
+
+    for i in range (1,10):
+        print(i)
+
+assing=counter()
+
+def gize (aasignment):
+
+    aasignment
+
+gize(assing)
+    
+
+
+#assing
