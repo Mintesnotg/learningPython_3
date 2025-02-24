ddb53843919e7dfac7085b614bf45c0a3ee46414
Mintesnot Girma <MintesnotGi@ethiopianairlines.com>
2025-02-24 16:57:24 +0300

    refresh2

---
+++ DSA/list.py
@@ -0,0 +1,42 @@
+cities=("Addis","Legos")
+
+cities=cities.__add__(("Cairo","Asmara"))
+
+for city in cities:
+    print(city)
+
+bob='Don\'t worry I don\'t have '
+bob=bob + '20\u20AC'
+
+# print ('''ethiopia is one of 
+#        the gratest nation in the 
+#        world''')
+my_set = {1, 2, 3, 4,1, 5}
+
+print(bob)
+name1= set("Mintei")
+print (' is found in the set ', 5 in my_set)
+print(name1)
+print(my_set)  # Output: {1, 2, 3, 4, 5}
+# names= set(bob)
+
+result = 27 /4
+
+print("float result ", result)
+
+for name in name1:
+    print( 'Name value', name)
+
+
+alpha =[1,2,3]
+
+beta=alpha
+
+beta+=[4,5]
+beta=beta+[6,7]
+
+
+print(alpha)
+
+
+
--- DSA/starting.py
+++ DSA/starting.py
@@ -1,1 +1,17 @@
 print ('Hello World !')
+temprature=10.9
+temprature=9.1
+# temprature='value'
+
+# temprature= temprature +'str '
+# temprature.
+
+print(temprature)
+# refr=None
+
+x=2.14
+print("Original value of x :" , x)
+
+y=3.14
+
+print(" IS x the same object as y" , x is y)
